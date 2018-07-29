f982cf27f9aad629c6a5249576c97da800eb325b
webfaria <web_faria@yahoo.com.br>
2018-07-29 15:17:48 -0300

    Lazy loading

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { IonicPage } from '../../../node_modules/ionic-angular/navigation/ionic-page';
 
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
